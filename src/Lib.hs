e5f8c7ea2b8b32f7098f6d087e5614089b6ddf1a
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2024-02-12 22:12:21 +0900

    HTTP Request のサンプル

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+
 module Lib
     ( someFunc
     ) where
+import Network.HTTP.Req
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Text (Text)
+import qualified Data.Text.IO as DTI (putStrLn)
 
+-- JSONを受取り値を取り出すサンプル
+-- see http://note.kurodigi.com/haskell-req/
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  runReq defaultHttpConfig request
+  where
+  -- 通信処理
+  request :: Req ()
+  request = do
+    res <- req GET
+               (https "httpbin.org" /: "ip")
+               NoReqBody
+               jsonResponse
+               mempty
+    let val = (responseBody res :: Value)
+    case val of
+      Object o ->
+        case parse getIp o of
+          Success r -> liftIO $ DTI.putStrLn r
+          Error e -> liftIO $ Prelude.putStrLn e
+
+  getIp :: Object -> Parser Text
+  getIp o = do o .: "origin"
